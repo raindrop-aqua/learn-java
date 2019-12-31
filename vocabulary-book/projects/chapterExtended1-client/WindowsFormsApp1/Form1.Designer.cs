af900f2779bd37e8bc8628172b223d27cffd72a0
Masahiro Atsumi <raindrop.aqua@gmail.com>
2020-01-01 01:44:16 +0900

    add chapter extended client. first commit.

---
+++ vocabulary-book/projects/chapterExtended1-client/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace WindowsFormsApp1 {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnTranslate = new System.Windows.Forms.Button();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.txtResultTranslate = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtQueryWord = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.txtResultAdd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNewWord = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.txtResultDelete = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDeleteWord = new System.Windows.Forms.TextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtNewTranslatedWord = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(344, 255);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.tableLayoutPanel1);
+            this.tabPage1.Controls.Add(this.btnTranslate);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(336, 229);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "翻訳";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.tableLayoutPanel2);
+            this.tabPage2.Controls.Add(this.btnAdd);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(336, 229);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "単語登録";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnTranslate
+            // 
+            this.btnTranslate.Location = new System.Drawing.Point(242, 193);
+            this.btnTranslate.Name = "btnTranslate";
+            this.btnTranslate.Size = new System.Drawing.Size(88, 30);
+            this.btnTranslate.TabIndex = 4;
+            this.btnTranslate.Text = "翻訳";
+            this.btnTranslate.UseVisualStyleBackColor = true;
+            this.btnTranslate.Click += new System.EventHandler(this.btnTranslate_Click);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.tableLayoutPanel3);
+            this.tabPage3.Controls.Add(this.btnDelete);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(336, 229);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "単語削除";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.txtResultTranslate, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtQueryWord, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(330, 107);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // txtResultTranslate
+            // 
+            this.txtResultTranslate.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtResultTranslate.Location = new System.Drawing.Point(3, 85);
+            this.txtResultTranslate.Name = "txtResultTranslate";
+            this.txtResultTranslate.ReadOnly = true;
+            this.txtResultTranslate.Size = new System.Drawing.Size(324, 19);
+            this.txtResultTranslate.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(0, 10, 0, 0);
+            this.label1.Size = new System.Drawing.Size(324, 22);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "翻訳したい語句を入力してください。";
+            // 
+            // txtQueryWord
+            // 
+            this.txtQueryWord.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtQueryWord.Location = new System.Drawing.Point(3, 60);
+            this.txtQueryWord.Name = "txtQueryWord";
+            this.txtQueryWord.Size = new System.Drawing.Size(324, 19);
+            this.txtQueryWord.TabIndex = 7;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.txtNewTranslatedWord, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.txtResultAdd, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.txtNewWord, 0, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(330, 141);
+            this.tableLayoutPanel2.TabIndex = 8;
+            // 
+            // txtResultAdd
+            // 
+            this.txtResultAdd.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtResultAdd.Location = new System.Drawing.Point(3, 119);
+            this.txtResultAdd.Name = "txtResultAdd";
+            this.txtResultAdd.ReadOnly = true;
+            this.txtResultAdd.Size = new System.Drawing.Size(324, 19);
+            this.txtResultAdd.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(0, 10, 0, 0);
+            this.label2.Size = new System.Drawing.Size(324, 22);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "追加したい語句を入力してください。";
+            // 
+            // txtNewWord
+            // 
+            this.txtNewWord.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtNewWord.Location = new System.Drawing.Point(3, 69);
+            this.txtNewWord.Name = "txtNewWord";
+            this.txtNewWord.Size = new System.Drawing.Size(324, 19);
+            this.txtNewWord.TabIndex = 7;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(242, 194);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(88, 30);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "登録";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.txtResultDelete, 0, 2);
+            this.tableLayoutPanel3.Controls.Add(this.label3, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.txtDeleteWord, 0, 1);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 3;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(330, 107);
+            this.tableLayoutPanel3.TabIndex = 8;
+            // 
+            // txtResultDelete
+            // 
+            this.txtResultDelete.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtResultDelete.Location = new System.Drawing.Point(3, 85);
+            this.txtResultDelete.Name = "txtResultDelete";
+            this.txtResultDelete.ReadOnly = true;
+            this.txtResultDelete.Size = new System.Drawing.Size(324, 19);
+            this.txtResultDelete.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label3.Location = new System.Drawing.Point(3, 0);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(0, 10, 0, 0);
+            this.label3.Size = new System.Drawing.Size(324, 22);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "削除したい語句を入力してください。";
+            // 
+            // txtDeleteWord
+            // 
+            this.txtDeleteWord.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtDeleteWord.Location = new System.Drawing.Point(3, 60);
+            this.txtDeleteWord.Name = "txtDeleteWord";
+            this.txtDeleteWord.Size = new System.Drawing.Size(324, 19);
+            this.txtDeleteWord.TabIndex = 7;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(242, 194);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(88, 30);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "削除";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtNewTranslatedWord
+            // 
+            this.txtNewTranslatedWord.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtNewTranslatedWord.Location = new System.Drawing.Point(3, 94);
+            this.txtNewTranslatedWord.Name = "txtNewTranslatedWord";
+            this.txtNewTranslatedWord.Size = new System.Drawing.Size(324, 19);
+            this.txtNewTranslatedWord.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 274);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox txtResultTranslate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtQueryWord;
+        private System.Windows.Forms.Button btnTranslate;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TextBox txtResultAdd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNewWord;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.TextBox txtResultDelete;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDeleteWord;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtNewTranslatedWord;
+    }
+}
+
---
+++ vocabulary-book/projects/chapterExtended1-client/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Thrift.Transport;
+using Thrift.Protocol;
+using com.example.vocabularybook.chapter_extended1.thrift;
+
+namespace WindowsFormsApp1 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btnTranslate_Click(object sender, EventArgs e) {
+            using (THttpClient transport = new THttpClient(new Uri("http://192.168.1.2:8080/api"))) {
+                TProtocol protocol = new TBinaryProtocol(transport);
+                BookApi.Client client = new BookApi.Client(protocol);
+
+                transport.Open();
+                txtResultTranslate.Text = client.search(txtQueryWord.Text);
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e) {
+            using (THttpClient transport = new THttpClient(new Uri("http://192.168.1.2:8080/api"))) {
+                TProtocol protocol = new TBinaryProtocol(transport);
+                BookApi.Client client = new BookApi.Client(protocol);
+
+                transport.Open();
+                txtResultAdd.Text = client.addword(txtNewWord.Text, txtNewTranslatedWord.Text);
+            }
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e) {
+            using (THttpClient transport = new THttpClient(new Uri("http://192.168.1.2:8080/api"))) {
+                TProtocol protocol = new TBinaryProtocol(transport);
+                BookApi.Client client = new BookApi.Client(protocol);
+
+                transport.Open();
+                txtResultDelete.Text = client.deleteWord(txtDeleteWord.Text);
+            }
+        }
+    }
+}
